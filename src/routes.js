08e4e62d9f0a9a1466d9b0adb6842b46f0a824c8
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-10-04 15:42:41 -0300

    Add nova rota

--- src/routes.js
+++ src/routes.js
@@ -12,8 +12,8 @@ import AppointmentController from "./app/controllers/AppointmentController"
 const upload = multer(multerConfig)
 const routes = new Router()
 
- routes.get("/services", ServiceController.index)
- routes.get("/categories", CategoryController.index)
+routes.get("/services", ServiceController.index)
+routes.get("/categories", CategoryController.index)
 
 //* ROTA PARA CRIAÇÃO DE USUÁRIOS */
 routes.post("/users", UserController.store)
@@ -21,24 +21,25 @@ routes.post("/users", UserController.store)
 //* ROTA PARA REALIZAR O LOGIN*/
 routes.post("/sessions", SessionController.store)
 
-//*AUTENTICAÇÃO COM JWT 
+//*AUTENTICAÇÃO COM JWT
 routes.use(authMiddleware)
 // ?(TODAS AS ROTAS ABAIXO DEPENDE DESSA AUTENTICAÇÃO)*/
 
 //* ROTAS DOS SERVIÇOS*/
- routes.post("/services", upload.single("file"), ServiceController.store)
- routes.get("/services", ServiceController.index)
- routes.put("/services/:id", upload.single("file"), ServiceController.update)
- routes.delete("/services/:id", upload.single("file"), ServiceController.delete)
+routes.post("/services", upload.single("file"), ServiceController.store)
+routes.get("/services", ServiceController.index)
+routes.put("/services/:id", upload.single("file"), ServiceController.update)
+routes.delete("/services/:id", upload.single("file"), ServiceController.delete)
 
 //*ROTAS DAS CATEGORIAS*/
- routes.post("/categories", upload.single("file"), CategoryController.store)
- routes.get("/categories", CategoryController.index)
- routes.put("/categories/:id", upload.single("file"), CategoryController.update)
+routes.post("/categories", upload.single("file"), CategoryController.store)
+routes.get("/categories", CategoryController.index)
+routes.put("/categories/:id", upload.single("file"), CategoryController.update)
+routes.delete("/categories/:id", upload.single("file"), CategoryController.delete)
 
 //* ROTAS DOS AGENDAMENTOS*/
- routes.post("/appointment", AppointmentController.store)
- routes.put("/appointment/:id", AppointmentController.update)
- routes.get("/appointment", AppointmentController.index)
+routes.post("/appointment", AppointmentController.store)
+routes.put("/appointment/:id", AppointmentController.update)
+routes.get("/appointment", AppointmentController.index)
 
 export default routes
